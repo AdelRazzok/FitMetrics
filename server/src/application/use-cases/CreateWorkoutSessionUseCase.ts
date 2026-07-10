83501f856be2fe1df611c410fd03e69b90ed7167
AdelRazzok <lhp6.arazzok@gmail.com>
2026-07-10 15:21:57 +0200

    server: refacto domain rules validation

--- server/src/application/use-cases/CreateWorkoutSessionUseCase.ts
+++ server/src/application/use-cases/CreateWorkoutSessionUseCase.ts
@@ -1,7 +1,6 @@
 import { CreateWorkoutSessionDTO, ACTIVITY_TYPES } from '@fitmetrics/shared'
 import { WorkoutSessionRepository } from '../../domain/ports/WorkoutSessionRepository'
-import { WorkoutSession } from '../../domain/entities/WorkoutSession'
-import { DomainError } from '../../domain/errors/DomainError'
+import { WorkoutSession, validateWorkoutSessionRules } from '../../domain/entities/WorkoutSession'
 
 export class CreateWorkoutSessionUseCase {
   constructor(private readonly repository: WorkoutSessionRepository) {}
@@ -9,50 +8,7 @@ export class CreateWorkoutSessionUseCase {
   async execute(data: CreateWorkoutSessionDTO): Promise<WorkoutSession> {
     data.title = data.title.trim()
 
-    if (data.title.length < 3 || data.title.length > 50) {
-      throw new DomainError(
-        'Le titre doit être compris entre 3 et 50 caractères.',
-        'title',
-      )
-    }
-
-    if (!ACTIVITY_TYPES.includes(data.activity)) {
-      throw new DomainError(
-        `L'activité '${data.activity}' n'est pas valide.`,
-        'activity',
-      )
-    }
-
-    if (
-      data.durationInSeconds <= 0 ||
-      !Number.isInteger(data.durationInSeconds)
-    ) {
-      throw new DomainError(
-        'La durée doit être un nombre entier strictement positif.',
-        'durationInSeconds',
-      )
-    }
-
-    const parsedDate = new Date(data.date)
-    if (isNaN(parsedDate.getTime())) {
-      throw new DomainError('Le format de la date est invalide.', 'date')
-    }
-    if (parsedDate > new Date()) {
-      throw new DomainError('La date ne peut pas être dans le futur.', 'date')
-    }
-
-    if (data.intensity !== undefined && data.intensity !== null) {
-      if (
-        !Number.isInteger(data.intensity) ||
-        data.intensity < 1 ||
-        data.intensity > 10
-      ) {
-        throw new DomainError(
-          "L'intensité doit être un entier compris entre 1 et 10.",
-          'intensity',
-        )
-      }
-    }
+    validateWorkoutSessionRules(data)
 
     return this.repository.create(data)
   }
